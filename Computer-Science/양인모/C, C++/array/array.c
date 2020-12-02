d3a58d99532244b588a08fe19e2bf8bc9a98d4d6
inmoG <inmoy@naver.com>
2020-12-02 22:22:25 +0900

    first refactoring

--- Computer-Science/양인모/C, C++/array/array.c
+++ Computer-Science/양인모/C, C++/array/array.c
@@ -1,47 +1,35 @@
 #include <stdio.h>
 
-int array_main()
+int main()
 {
-    /*
-    문장에 대문자가 있다면 소문자로 변환하는 프로그램
-    */
 	char upper_case[30];
 	int count = 0;
-	int casting = 0;
-	gets(upper_case);
+	gets(upper_case); // 문장을 입력 받기 위해 gets 사용
 
+    /*
+    1. char은 정수형이다.
+    2. if eles if else 구문을 적절하게 사용하자, if else를 2개 쓰는 건 비효율적이다.
+    3. 중복되는 코드는 항상 제거하자
+    4. 불필요한 변수를 사용하지 말자
+    */
 	for (int i = 0; i < sizeof(upper_case) / sizeof(upper_case[0]); i++) // 30번째 인덱스까지 검사
 	{
-		if (upper_case[i] == '\0')
+		if (upper_case[i] == '\0') // i번째 인덱스가 '\0'이면 for문을 종료한다.
 
 		{
 			break;
 		}
-
-		casting = (int)upper_case[i]; // i번째 인덱스 값을 정수로 바꾼다. 
-
-		if (casting < 65 || casting > 90) // A(65) ~ Z(90) 가 아닌 값은 특수문자, 소문자이다. 대문자인 값만 변경해야 하므로 for문으로 올라간다.
+		else if (upper_case[i] < 65 || upper_case[i] > 90) // A(65) ~ Z(90) 가 아닌 값은 특수 문자, 소문자이다.
 		{
 			continue;
 		}
 		else
 		{
-
-	
-			// 1. i번째 값이 65 ~ 90이면 대문자이다. 따라서 아래 반복문 필요 없음
-			for (int ASCII = 65; ASCII <= casting; ASCII++) // casting과 ASCII 값이 같은지 확인하기 위한 반복문
-			{
-				if (casting == ASCII) 
-				{
-					upper_case[i] = ASCII + 32; // 같다면 
-					count++;
-				}
-			}
-			
+			upper_case[i] += 32; // 대문자면 32 추가해 소문자로 변환 
+			count++;
 		}
-
-
 	}
+
 	printf("%s\n", upper_case);
 	printf("%d\n", count);
 	return 0;
